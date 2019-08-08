901a58131907a24251bc2b28685d0f8178b7311a
Dylan Fouche <dylanfouche@gmail.com>
2019-08-08 11:47:43 +0200

    Implemented grid, changed farming routine, visualised agent positions

--- src/egypt.py
+++ src/egypt.py
@@ -1,25 +1,43 @@
 import sys
 from model import *
 import matplotlib.pyplot as plt
+import traceback
+import numpy as np
 
 def main():
 	#get command line params
-	if(len(sys.argv)!=3):
-		print("Usage:\t$python3 ./src/egypt.py [num_agents] [num_steps]")
+	if(len(sys.argv)!=5):
+		print("Usage:\t$python3 ./src/egypt.py [num_agents] [num_steps] [grid_width] [grid_height]")
 		quit()
 	else:
 		num_agents = int(sys.argv[1])
 		num_steps = int(sys.argv[2])
+		grid_width = int(sys.argv[3])
+		grid_height = int(sys.argv[4])
 	#create and run model
-	model = egyptModel(num_agents)
+	model = egyptModel(num_agents, grid_width, grid_height)
 	for i in range(num_steps):
 		model.step()
 	#visualise agent wealth
+	plt.subplot(1,2,1)
 	agent_wealth = [agent.wealth for agent in model.schedule.agents]
-	plt.hist(agent_wealth)
+	colours = ["alive" if agent.wealth > 0 else "dead" for agent in model.schedule.agents]
+	N, bins, patches = plt.hist(agent_wealth)
 	plt.title("Agent wealth distribution")
 	plt.xlabel("Wealth")
 	plt.ylabel("Agents")
+	for i,rect in enumerate(patches):
+		patches[i].set_color('r') if rect.get_x()<0 else patches[i].set_color('g')
+	#visualise agent position
+	plt.subplot(1,2,2)
+	agent_counts = np.zeros((model.grid.width, model.grid.height))
+	for cell in model.grid.coord_iter():
+	    cell_content, x, y = cell
+	    agent_count = len(cell_content)
+	    agent_counts[x][y] = agent_count
+	plt.imshow(agent_counts, interpolation='nearest')
+	plt.colorbar()
+	plt.title("Agent positions")
 	plt.show()
 
 if __name__ == "__main__":
@@ -28,7 +46,7 @@ if __name__ == "__main__":
 	except KeyboardInterrupt:
 		print("Exiting...")
 		quit()
-	except e:
-		print("Some other error occurred")
-		print(e.message)
+	except Exception as e:
+		print("Some other error occurred:")
+		print(traceback.print_exc())
 		quit()
--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from mesa.space import MultiGrid
 
 class egyptAgent(Agent):
     '''A household with random initial wealth'''
@@ -14,24 +15,35 @@ class egyptAgent(Agent):
         self.eat()
 
     def farm(self):
-        # TODO
-        self.wealth += self.random.randint(0, 2475) #2475 is maximum yield from netlogo
+        # grain yield is inversely proportional to distance from column x=0 (river)
+        x, y = self.pos
+        width = self.model.grid.width
+        self.wealth += (2475/width) * (width-x) #2475 is maximum yield from netlogo
+        #for x=0 => yield=max. for x=width => yield=0.
 
     def eat(self):
         # TODO
-        self.wealth -= self.random.randint(0,10)*160 #160 is annual grain consumption per person from netlogo
+        self.wealth -= self.random.randint(1,6)*160 #160 is annual grain consumption per person from netlogo
+        #number of agents in household unknown, chosen at random between 1 and 6.
 
 class egyptModel(Model):
     '''A model that aggregates n agents'''
 
-    def __init__(self, n):
+    def __init__(self, n, w, h):
         self.num_agents = n
-        # Add scheduler
+        # Create scheduler
         self.schedule = RandomActivation(self)
+        # Create grid
+        self.grid = MultiGrid(w, h, False) #grid is not toroidal
         # Create agents
         for i in range(self.num_agents):
             agent = egyptAgent(i, self)
+            # add agent to scheduler
             self.schedule.add(agent)
+            # add agent to grid
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(agent, (x, y))
 
     def step(self):
         '''Advance the model by one tick.'''
