2144484582d0af6ae7bbe8c1a77ef0bbda4557ec
Stuart Mesham <stumes3@gmail.com>
2019-08-11 18:51:49 +0200

    Gini coefficient division by 0 fix

--- src/egypt_model.py
+++ src/egypt_model.py
@@ -19,6 +19,10 @@ def compute_gini(model):
     agent_wealths = [agent.grain for agent in model.schedule.agents]
     x = sorted(agent_wealths)
     N = model.num_agents
+    
+    if N * sum(x) == 0:
+        return 0
+    
     B = sum(xi * (N - i) for i, xi in enumerate(x)) / (N * sum(x))
     return (1 + (1 / N) - 2 * B)
 
