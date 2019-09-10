ce1319d0fe51b931e9c9ddbe100725debbe9798e
Dylan Fouche <dylanfouche@gmail.com>
2019-09-10 03:39:01 +0200

    Bug fix for run_simulation.py

--- run_simulation.py
+++ run_simulation.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import inspect
 
 if len(sys.argv) != 3:
     print("Usage:\tpython3 run_simulation.py [grid_width] [grid_height]")
@@ -9,11 +10,9 @@ if len(sys.argv) != 3:
 else:
     grid_width = sys.argv[1]
     grid_height = sys.argv[2]
-    run_cmd = "python3 src/simulations/gui_simulation.py " + grid_width + " " + grid_height + " > /dev/null"
+    run_cmd = "export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/src && python3 src/simulations/gui_simulation.py " + grid_width + " " + grid_height + " > /dev/null"
 
     print("Checking dependencies...")
     os.system("pip3 install -r requirements.txt > /dev/null")
-    print("Updating pythonpath...")
-    os.system("export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/src > /dev/null")
     print("Launching simulation...")
     os.system(run_cmd)
